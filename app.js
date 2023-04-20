22af42dd42a64bd6bc61bf5fb1d98882b8cb54c8
Justin Agim <justinagim@gmail.com>
2023-04-19 20:24:42 -0500

    Here Partial Lab

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
 "use strict";
 
-let answerOne = prompt("How you doing?").toLowerCase();
+let answerOne = prompt("Are you doing good?").toLowerCase();
 
 if (answerOne === "yes" || answerOne === "y") {
   alert("I'm doing good.");
+  //console.log("I'm doing fine.")
 } else {
-  console.log("Im doing bad.");
+  alert("I'm ok.")
+  //console.log("Im doing bad.");
 }
 
-let answerTwo = prompt("What is going on today?").toLowerCase();
+let answerTwo = prompt("Is it snowing today?").toLowerCase();
 
 if (answerTwo === "no" || answerTwo === "n") {
   alert("it's sunny.");
+  //console.log("No, it is not snowing today.");
 } else {
   alert("it's raining.");
+  //console,log('it is hot today.');
+
 }
 
-let answerThree = prompt("how is your work?").toLowerCase();
+let answerThree = prompt("do I go to work?").toLowerCase();
 
 if (answerThree === "yes" || answerThree === "y") {
   alert("It is fine.");
+  //console.log("yes, I go to work.");
 } else {
-  console.log("It is worse.");
-}
-  let answerFour = prompt("Where do I live?").toLowerCase();
+  alert("It is worse.");
+  // console.log("It is worse.");
+it}
+  let answerFour = prompt("Do I live in Memphis?").toLowerCase();
 
   if (answerFour === "no" || answerFour === "n") {
     alert("I live in Nashville.");
+    //console.log("No, I do not live in Memphis.");
   } else {
-    alert("I do not live in Memphis.");
+    alert("I do not live in Knoxville.");
+    console.log("I dont live there.");
   }
 
-  let answerFive = prompt("How old am I?").toLowerCase();
+  let answerFive = prompt("Am I 38 years old?").toLowerCase();
 
   if (answerFive === "yes" || answerFive === "y") {
-    alert("I am 38 years old.");
+    alert("yes,I am 38 years old.");
+    //console.log("yes,I am 38 years old.")
   } else {
-    console.log("I dont know.");
+    alert ("That's wrong, I am 38 years old.")
+   // console.log("I dont know.");
   }
